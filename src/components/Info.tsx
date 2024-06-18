57e03e4df534624c98bb6a99d29de40e88a67d26
ldsoftware <daniele.lacanale@ldsoftware.eu>
2024-06-18 18:04:41 +0800

    modificata pagina info

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -5,9 +5,10 @@ export default function Info(){
     return(<>
         <Menu />
         <br></br>
-        <div className="info mt-5">
-            <p>Server : {server}</p>
+        <div className="info mt-5 text-center">
+            <p>Author : Lacanale Daniele</p>
             <p>ver {version}</p>
+            <p>Server : {server}</p>
         </div>        
     </>)
 }
